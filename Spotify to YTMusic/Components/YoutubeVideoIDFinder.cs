71ef47b21fcbca2fbe51ab8290009f1182beeffe
HualiangZ <zhualiang88@gmail.com>
2025-07-29 16:20:04 +0100

    find videoID

---
+++ Spotify to YTMusic/Components/YoutubeVideoIDFinder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Spotify_to_YTMusic.Components
+{
+
+    internal class YoutubeVideoIDFinder
+    {
+        private static string fileoutput = @"./youtubeText.html";
+
+        public static void StoreHTML(string url)
+        {
+            var awaiter = CallURL(url);
+            if (awaiter.Result != "")
+            {
+                File.WriteAllText(fileoutput, awaiter.Result);
+                Console.WriteLine("output compleated");
+            }
+        }
+
+        public static string GetVideoId()
+        {
+            string html = File.ReadAllText(fileoutput);
+            string pattern = "\"videoId\":\"([a-zA-Z0-9_-]{11})\"";
+
+            Match match = Regex.Match(html, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+
+        private static async Task<string> CallURL(string url)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(url);
+            return await response;
+        }
+    }
+}
--- Spotify to YTMusic/Program.cs
+++ Spotify to YTMusic/Program.cs
@@ -1,32 +1,20 @@
-﻿using System.Net;
+﻿using Spotify_to_YTMusic.Components;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Spotify_to_YTMusic
 {
     internal class Program
     {
-        private static string fileoutput = @"./youtubeText.txt";
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            string url = "https://www.youtube.com/results?search_query=jump+by+blackpink+%22topic%22";
-            var awaiter = CallURL(url);
-            if (awaiter.Result != "")
-            {
-                File.WriteAllText(fileoutput, awaiter.Result);
-                Console.WriteLine("output compleated");
-                awaiter.Result
-            }
+            YoutubeVideoIDFinder.StoreHTML("https://www.youtube.com/results?search_query=jump+by+blackpink+%22topic%22");
+            var videoID = YoutubeVideoIDFinder.GetVideoId();
+            Console.WriteLine(videoID);
             Console.ReadKey();
-            
         }
 
-        public static async Task<string> CallURL(string url)
-        {
-            HttpClient client = new HttpClient();
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
-            client.DefaultRequestHeaders.Accept.Clear();
-            var response = client.GetStringAsync(url);
-            return await response;
-        }
+        
     }
 }
