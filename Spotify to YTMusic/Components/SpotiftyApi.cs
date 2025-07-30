15c005d558210b890939119edcc71fc9c2a2f2de
HualiangZ <zhualiang88@gmail.com>
2025-07-30 15:20:02 +0100

    learning Spotify api

---
+++ Spotify to YTMusic/Components/SpotiftyApi.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_to_YTMusic.Components
+{
+    internal class SpotiftyApi
+    {
+        public string? AccessToken { get; set; }
+        private string url = "https://api.spotify.com/v1/";
+        public void GetAccessToken()
+        {
+            string url = "https://accounts.spotify.com/api/token";
+            var clientId = "";
+            var clientSecret = "";
+
+            var encodeClientId_ClientSecret = Convert.ToBase64String((Encoding.UTF8.GetBytes(string.Format("{0}:{1}", clientId, clientSecret))));
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Accept = "application/json";
+            webRequest.Headers.Add("Authorization: Basic " + encodeClientId_ClientSecret);
+
+            var request = ("grant_type=client_credentials");
+            byte[] req_bytes = Encoding.ASCII.GetBytes(request);
+            webRequest.ContentLength = req_bytes.Length;
+
+            Stream stream = webRequest.GetRequestStream();
+            stream.Write(req_bytes, 0, req_bytes.Length);
+            stream.Close();
+
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            string jsonString = "";
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+            jsonString = reader.ReadToEnd();
+            reader.Close();
+
+            JObject json = JObject.Parse(jsonString);
+            AccessToken = (string)json.GetValue("access_token");
+            
+        }
+
+        public void GetPlaylist()
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url + "playlists/5a7q5av1kX3ewlMwGuaQE3/tracks?offset=100&limit=100");
+            webRequest.Method = "GET";
+            webRequest.Headers.Add("Authorization: Bearer " + AccessToken);
+
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            string jsonString = "";
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+            jsonString = reader.ReadToEnd();
+            reader.Close();
+            JObject json = JObject.Parse(jsonString);
+            Console.Write(json);
+        }
+    }
+}
--- Spotify to YTMusic/Program.cs
+++ Spotify to YTMusic/Program.cs
@@ -1,15 +1,19 @@
-﻿using Spotify_to_YTMusic.Components;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Spotify_to_YTMusic.Components;
+using System.Buffers.Text;
 using System.Net;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
 
 namespace Spotify_to_YTMusic
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            YoutubeVideoIDFinder.StoreHTML("https://www.youtube.com/results?search_query=jump+by+blackpink+%22topic%22");
+            /*YoutubeVideoIDFinder.StoreHTML("https://www.youtube.com/results?search_query=jump+by+blackpink+%22topic%22");
             var videoID = YoutubeVideoIDFinder.GetVideoId();
             if(videoID != "")
             {
@@ -20,9 +24,12 @@ namespace Spotify_to_YTMusic
                 Console.WriteLine("no ID found");
             }
 
-                Console.ReadKey();
+            Console.ReadKey();*/
+            SpotiftyApi api = new SpotiftyApi();
+            api.GetAccessToken();
+            api.GetPlaylist();
+            
         }
 
-        
     }
 }
