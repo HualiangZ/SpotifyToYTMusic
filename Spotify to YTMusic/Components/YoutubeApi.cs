6f14dee5ee79031786018f0c214d57c9fa33a305
HualiangZ <zhualiang88@gmail.com>
2025-08-10 10:55:48 +0100

    init youtube api class

---
+++ Spotify to YTMusic/Components/YoutubeApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+namespace Spotify_to_YTMusic.Components
+{
+    internal class YoutubeApi
+    {
+        UserCredential user;
+
+
+    }
+}
