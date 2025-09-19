a09f71024ddf586ded4cc2543899fc3c899bfa33
HualiangZ <zhualiang88@gmail.com>
2025-09-19 14:50:20 +0100

    init Spotify To YouTube Sync

---
+++ Spotify to YTMusic/Components/SpotifyToYouTubeSync.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_to_YTMusic.Components
+{
+    internal class SpotiftToYouTubeSync
+    {
+        public void SyncPlaylist()
+        {
+
+        }
+    }
+}
