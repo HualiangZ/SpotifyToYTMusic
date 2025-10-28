ad41e256f438765b4ed6054172c9cc0b013bf112
HualiangZ <zhualiang88@gmail.com>
2025-10-28 13:18:32 +0000

    Update SpotifyToYouTubeSync.cs

--- Spotify to YTMusic/Components/SpotifyToYouTubeSync.cs
+++ Spotify to YTMusic/Components/SpotifyToYouTubeSync.cs
@@ -28,11 +28,12 @@ namespace Spotify_to_YTMusic.Components
         public async Task SyncPlaylistAsyncWithSpotifyID(string spotifyPlaylistId)
         {
             string playlistName = await spotifyApi.StorePlaylistToDB(spotifyPlaylistId).ConfigureAwait(false);
-
+            await spotifyApi.StorePlaylistInfoToDBAsync(spotifyPlaylistId).ConfigureAwait(false);
             string youtubePlaylistId = MusicDBApi.GetSyncedPlaylistWithSpotify(spotifyPlaylistId);
+
             if (youtubePlaylistId == null)
             {
-                await spotifyApi.StorePlaylistInfoToDBAsync(spotifyPlaylistId).ConfigureAwait(false);
+                
                 string playlistId = youtubePlaylistId;
                 string newYoutubePlaylistId = await youtubeApi.CreateNewPlaylist(playlistName).ConfigureAwait(false);
                 PlaylistSync newPlaylistSync = new PlaylistSync();
